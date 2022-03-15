04d3684dd6c8a740247d3b3a317ad47d6db4e341
parameswar116 <101231739+parameswar116@users.noreply.github.com>
2022-03-15 23:10:40 +0530

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,8 @@
 import React from "react"
 
  const Game = ({ onClick, count, score }) => {
-   const newScore = + 1    // newscore = score+1
-   const newCount = + 1     //newcount = count+1
+   const newScore = score + 1    // newscore = score+1
+   const newCount = count + 1     //newcount = count+1
    return (
        <button onClick={() => onClick(newScore, newCount)}>Score</button>
    )
